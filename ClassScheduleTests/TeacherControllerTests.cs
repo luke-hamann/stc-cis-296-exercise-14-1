a7a4e052d63249f595b288819d228c49ec6afd5a
Luke Hamann <135341515+luke-hamann@users.noreply.github.com>
2025-02-18 09:07:04 -0600

    Add step 18.

--- ClassScheduleTests/TeacherControllerTests.cs
+++ ClassScheduleTests/TeacherControllerTests.cs
@@ -19,7 +19,17 @@ namespace ClassScheduleTests
         [Fact]
         public void IndexActionMethod_ModelIsAListOfTeacherObjects()
         {
+            // arrange
+            var rep = new Mock<IRepository<Teacher>>();
+            rep.Setup(m => m.List(It.IsAny<QueryOptions<Teacher>>()))
+                        .Returns(new List<Teacher>());
+            var controller = new TeacherController(rep.Object);
 
+            // act
+            var model = controller.Index().ViewData.Model;
+
+            // assert
+            Assert.IsType<List<Teacher>>(model);
         }
     }
 }
