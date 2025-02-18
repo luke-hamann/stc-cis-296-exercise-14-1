5ce92243da27205532629f8e0e1a20399ddda928
Luke Hamann <135341515+luke-hamann@users.noreply.github.com>
2025-02-18 08:31:04 -0600

    Add step 14.

--- ClassScheduleTests/TeacherControllerTests.cs
+++ ClassScheduleTests/TeacherControllerTests.cs
@@ -5,7 +5,15 @@ namespace ClassScheduleTests
         [Fact]
         public void IndexActionMethod_ReturnsAViewResult()
         {
+            // arrange
+            var rep = new Mock<IRepository<Teacher>>();
+            var controller = new TeacherController(rep.Object);
 
+            // act
+            var result = controller.Index();
+
+            // assert
+            Assert.IsType<ViewResult>(result);
         }
     }
 }
