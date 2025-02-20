6157f387378e7d95ac34ba2cf66836486a8c583a
Luke Hamann <135341515+luke-hamann@users.noreply.github.com>
2025-02-20 11:05:58 -0600

    Add step 23.

--- ClassScheduleTests/HomeControllerTests.cs
+++ ClassScheduleTests/HomeControllerTests.cs
@@ -1,11 +1,36 @@
-﻿namespace ClassScheduleTests
+﻿using Microsoft.AspNetCore.Http;
+
+namespace ClassScheduleTests
 {
     public class HomeControllerTests
     {
         [Fact]
         public void IndexActionMethod_ReturnsAViewResult()
         {
+            // arrange
+            var rep = new Mock<IClassScheduleUnitOfWork>();
 
+            var days = new Mock<IRepository<Day>>();
+            days.Setup(m => m.List(It.IsAny<QueryOptions<Day>>()))
+                .Returns(new List<Day>());
+
+            var classes = new Mock<IRepository<Class>>();
+            classes.Setup(m => m.List(It.IsAny<QueryOptions<Class>>()))
+                .Returns(new List<Class>());
+
+            rep.Setup(m => m.Days).Returns(days.Object);
+            rep.Setup(m => m.Classes).Returns(classes.Object);
+
+            var controller = new HomeController(rep.Object);
+
+            controller.ControllerContext.HttpContext = new DefaultHttpContext();
+            controller.ControllerContext.HttpContext.Session = new Mock<ISession>().Object;
+
+            // act
+            var result = controller.Index(1);
+
+            // assert
+            Assert.IsType<ViewResult>(result);
         }
     }
 }
